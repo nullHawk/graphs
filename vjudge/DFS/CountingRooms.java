93fae2e9b84656a04fc98e5e88192f9fc9f1ee43
Suryansh <suryanshsinghshakya1@gmail.com>
2025-05-02 10:09:53 +0000

    add: DFS/CountingRooms

---
+++ vjudge/DFS/CountingRooms.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class CountingRooms{
+    public static void main(String[] main){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.nextLine();
+        char[][] map = new char[n][m];
+        boolean[][] visited = new boolean[n][m];
+
+        for(int i = 0; i < n; i++){
+            String line = sc.nextLine();
+            map[i] = line.toCharArray();
+        }
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(!visited[i][j] && map[i][j] == '.'){
+                    dfs(map, visited, i, j, n, m);
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    static void dfs(char[][] map, boolean[][] visited, int currI, int currJ, int n, int m){
+        if(currI == n || currJ == m || map[currI][currJ] == '#' || visited[currI][currJ]){
+            return;
+        }
+        visited[currI][currJ] = true;
+        dfs(map, visited, currI+1, currJ, n, m);
+        dfs(map, visited, currI-1, currJ, n, m);
+        dfs(map, visited, currI, currJ-1, n, m);
+        dfs(map, visited, currI, currJ+1, n, m);
+    }
+}
